989fd8536621598456a3a50a05de0c8df4585c67
Juan Andres Sanchez Lopez <erinur@gmail.com>
2023-08-10 14:38:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function check() {
     document.getElementById('container').innerHTML = '<div id="response"><img id="response-img" src="./assets/images/icon-success.svg" alt=""><h1>Thank for subscribing!</h1><p>A confirmation email have been sent to <span>' + expresion + '</span>. Please open it and click the button inside to confirm your subscription</p><input type="button" value="Dismiss message" onclick="location.reload()"></div>'
     document.getElementById('container').style.width = '31rem';
     document.getElementById('container').style.height = '32rem';
+    document.main.style.width = '1440px';
+    document.main.style.height = '1080px';
   }
 }
